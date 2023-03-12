6fe22937c723583d51530901b5585eca849e1d2d
Lohan Pitta <lohanpitta1@gmail.com>
2023-03-12 15:43:50 -0300

    Carrinho implementado junto com o botão de aumento

--- App/Controllers/CarrinhoController.php
+++ App/Controllers/CarrinhoController.php
@@ -9,13 +9,44 @@ use \MF\Model\Container;
 class CarrinhoController extends Action {
 
     public function carrinho() {
+        session_start();
+        
+        $carrinho = Container::getModel('Carrinho');
+
+        $carrinho->__set('usuario_id', $_SESSION['id']);
+        
+        // echo '<pre>';
+        // print_r($carrinho->getAll());
+        // echo '</pre>';
+
+        if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            echo $carrinho->getAll();
+            exit;
+        }
+
+        $this->view->hamburguers = $carrinho->getAll();
+
         $this->render('carrinho');
     }
 
     public function adicionarAoCarrinho() {
+        session_start();
+
         $carrinho = Container::getModel('Carrinho');
         
-        echo json_encode($_POST[]);
+        $carrinho->__set('hamburguer_id', $_POST['id_hamburguer']);
+        $carrinho->__set('usuario_id', $_SESSION['id']);
+        $verificacao = $carrinho->verificaHamburguer();
+
+        if($verificacao) {
+            $carrinho->updateQuantidade();
+        } else {
+            $carrinho->adicionar();
+        }
+
+        echo json_encode($carrinho->hamburguerAtualizado());
+        
+        // echo 'Seu hamburguer foi adicionado ao carrinho!!!';
     }
     
     
--- App/Models/Carrinho.php
+++ App/Models/Carrinho.php
@@ -5,7 +5,120 @@ namespace App\Models;
 use MF\Model\Model;
 use PDO;
 
-class Carrinho extends Model {
+class Carrinho extends Model
+{
 
+    private $hamburguer_id;
+    private $usuario_id;
+
+    public function __get($atributo)
+    {
+        return $this->$atributo;
+    }
+
+    public function __set($atributo, $valor)
+    {
+        $this->$atributo = $valor;
+    }
+
+    function getAll()
+    {
+        $query = '
+            SELECT
+                h.id,
+                h.nome,
+                h.descricao,
+                c.valor,
+                c.quantidade
+            FROM
+                carrinho as c
+            LEFT JOIN
+                hamburguer as h ON (c.id_hamburguer = h.id)
+            WHERE
+                c.id_usuario = :usuario_id
+        ';
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':usuario_id', $this->__get('usuario_id'));
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function verificaHamburguer()
+    {
+        $query = '
+            SELECT 
+                *
+            FROM
+                carrinho
+            WHERE
+                id_usuario = :usuario_id AND
+                id_hamburguer = :hamburguer_id
+        ';
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':usuario_id', $this->__get('usuario_id'));
+        $stmt->bindValue(':hamburguer_id', $this->__get('hamburguer_id'));
+        $stmt->execute();
+
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function adicionar()
+    {
+        $query = '
+            INSERT INTO carrinho(id_usuario, id_hamburguer, quantidade, valor)
+            VALUES(
+                :usuario_id,
+                :hamburguer_id,
+                1,
+                (SELECT valor FROM hamburguer WHERE id = :hamburguer_id)
+            )
+        ';
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':usuario_id', $this->__get('usuario_id'));
+        $stmt->bindValue(':hamburguer_id', $this->__get('hamburguer_id'));
+        $stmt->execute();
+    }
+
+    function updateQuantidade() {
+        $query = '
+            UPDATE 
+                carrinho
+            SET 
+                quantidade = quantidade + 1, 
+                valor = (SELECT valor FROM hamburguer WHERE id = :hamburguer_id) * (quantidade)
+            WHERE 
+                id_usuario = :usuario_id AND id_hamburguer = :hamburguer_id;
+            
+            SELECT * FROM carrinho WHERE id_usuario = :usuario_id AND id_hamburguer = :hamburguer_id
+        ';
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':hamburguer_id', $this->__get('hamburguer_id'));
+        $stmt->bindValue(':usuario_id', $this->__get('usuario_id'));
+        $stmt->execute();
+        $stmt->closeCursor();
+
+    }
+
+    function hamburguerAtualizado() {
+        $query = '
+                SELECT 
+                    * 
+                FROM 
+                    carrinho 
+                WHERE 
+                    id_usuario = :usuario_id AND id_hamburguer = :hamburguer_id
+            ';
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':hamburguer_id', $this->__get('hamburguer_id'));
+        $stmt->bindValue(':usuario_id', $this->__get('usuario_id'));
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
     
 }
